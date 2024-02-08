3a40ccb475dde4ccbf598cfb3356d7f45fee9273
NessRnD <nessrnd@gmail.com>
2024-02-09 00:13:57 +0300

    commit#3

--- Assets/Scripts/Enemy_Golem/GolemAI.cs
+++ Assets/Scripts/Enemy_Golem/GolemAI.cs
@@ -5,50 +5,43 @@ public class GolemAI : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private float startWaitTime = 4f;
-    [SerializeField] private float timeToRotate = 2f;
     [SerializeField] private float speedWalk = 6f;
     [SerializeField] private float speedRun = 9f;
-
     [SerializeField] private float viewRadius = 15f;
     [SerializeField] private float viewAngle = 90f;
     [SerializeField] private LayerMask playerMask;
     [SerializeField] private LayerMask obstacleMask;
-
     [SerializeField] private Transform[] wayPoints;
-    private int m_CurrentWayPointIndex;
     
-    private Vector3 playerLastPosition = Vector3.zero;
-    private Vector3 m_PlayerPosition;
+    private int currentWayPointIndex;
+    private Vector3 playerPosition;
+    private float waitTime;
+    private bool playerInRange;
+    private bool isPatrol;
 
-    private float m_WaitTime;
-    private float m_TimeToRotate;
-    private bool m_PlayerInRange;
-    private bool m_PlayerNear;
-    private bool m_IsPatrol;
-    private bool m_CaughtPlayer;
+    private Animator anim;
 
     private void Start()
     {
-        m_PlayerPosition=Vector3.zero;
-        m_IsPatrol = true;
-        m_CaughtPlayer = false;
-        m_PlayerInRange = false;
-        m_WaitTime = startWaitTime;
-        m_TimeToRotate = timeToRotate;
+        anim = GetComponent<Animator>();
+        
+        playerPosition=Vector3.zero;
+        isPatrol = true;
+        playerInRange = false;
+        waitTime = startWaitTime;
 
-        m_CurrentWayPointIndex = 0;
+        currentWayPointIndex = 0;
         navMeshAgent = GetComponent<NavMeshAgent>();
 
-        navMeshAgent.isStopped = false;
-        navMeshAgent.speed = speedWalk;
-        navMeshAgent.SetDestination(wayPoints[m_CurrentWayPointIndex].position);
+        Move(speedWalk);
+        navMeshAgent.SetDestination(wayPoints[currentWayPointIndex].position);
     }
 
     private void Update()
     {
         EnvironmentView();
 
-        if (!m_IsPatrol)
+        if (!isPatrol)
         {
             Chasing();
         }
@@ -60,153 +53,104 @@ public class GolemAI : MonoBehaviour
 
     private void Chasing()
     {
-        m_PlayerNear = false;
-        playerLastPosition = Vector3.zero;
-        if (!m_CaughtPlayer)
-        {
-            Move(speedRun);
-            navMeshAgent.SetDestination(m_PlayerPosition);
-        }
-
-
+        //playerLastPosition = Vector3.zero;
         if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
-            //Debug.Log(navMeshAgent.remainingDistance);
-            if (m_WaitTime <=0 && !m_CaughtPlayer && Vector3.Distance(transform.position, 
-                GameObject.FindGameObjectWithTag("Player").transform.position) >= 6f)
-            {
-                m_IsPatrol = true;
-                m_PlayerNear = false;
-                Move(speedWalk);
-                m_TimeToRotate = timeToRotate;
-                m_WaitTime = startWaitTime;
-                navMeshAgent.SetDestination(wayPoints[m_CurrentWayPointIndex].position);
-            }
-            else
-            {
-                if (Vector3.Distance(transform.position,
-                    GameObject.FindGameObjectWithTag("Player").transform.position) <= 2.5f)
-                {
-                    Stop();
-                    Debug.Log("stop");
-                    m_WaitTime -= Time.deltaTime;
-                }
-            }
-        }
-    }
-
-    private void Patroling()
-    {
-        if (m_PlayerNear)
-        {
-            if (m_TimeToRotate <= 0)
-            {
-                Move(speedWalk);
-                LookingPlayer(playerLastPosition);
-            }
-            else
-            {
-                Stop();
-                m_TimeToRotate -= Time.deltaTime;
-            }
+            Stop();
+            anim.SetBool("Attack", true);
         }
         else
         {
-            m_PlayerNear = false;
-            playerLastPosition = Vector3.zero;
-            navMeshAgent.SetDestination(wayPoints[m_CurrentWayPointIndex].position);
-            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-            {
-                if (m_WaitTime <= 0)
-                {
-                    NextPoint();
-                    Move(speedWalk);
-                    m_WaitTime = startWaitTime;
-                }
-                else
-                {
-                    Stop();
-                    m_WaitTime -= Time.deltaTime;
-                }
-            }
+            anim.SetBool("Attack", false);
+            Move(speedRun);
+            navMeshAgent.SetDestination(playerPosition);
         }
     }
 
     private void Move(float speed)
     {
+        anim.SetBool("Walk",true);
+        if (isPatrol)
+        {
+            anim.SetBool("Victory",false);
+        }
         navMeshAgent.isStopped = false;
         navMeshAgent.speed = speed;
     }
     
     private void Stop()
     {
+        anim.SetBool("Walk",false);
+        if (isPatrol)
+        {
+            anim.SetBool("Victory",true);
+        }
         navMeshAgent.isStopped = true;
         navMeshAgent.speed = 0;
     }
 
     private void NextPoint()
     {
-        m_CurrentWayPointIndex = (m_CurrentWayPointIndex + 1) % wayPoints.Length;
-        navMeshAgent.SetDestination(wayPoints[m_CurrentWayPointIndex].position);
+        currentWayPointIndex = (currentWayPointIndex + 1) % wayPoints.Length;
+        navMeshAgent.SetDestination(wayPoints[currentWayPointIndex].position);
     }
-
-
-    private void CaughtPlayer()
+    
+    // патрулирование
+    private void Patroling()
     {
-        m_CaughtPlayer = true;
-    }
-
-    private void LookingPlayer(Vector3 player)
-    {
-        navMeshAgent.SetDestination(player);
-        if (Vector3.Distance(transform.position, player) <= 0.3f)
+        navMeshAgent.SetDestination(wayPoints[currentWayPointIndex].position);
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
         {
-            if (m_WaitTime <= 0)
+            if (waitTime <= 0)
             {
-                m_PlayerNear = false;
+                NextPoint();
                 Move(speedWalk);
-                navMeshAgent.SetDestination(wayPoints[m_CurrentWayPointIndex].position);
-                m_WaitTime = startWaitTime;
-                m_TimeToRotate = timeToRotate;
+                waitTime = startWaitTime;
             }
             else
             {
                 Stop();
-                m_WaitTime -= Time.deltaTime;
+                Debug.Log("patroling stop2");
+                waitTime -= Time.deltaTime;
             }
         }
     }
-
+    
+    /*входная точка скрипта*/
     private void EnvironmentView()
     {
+        //поиск игрока
         Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
-
+        //не входит в цикл пока не найдет игрока
         for (int i = 0; i < playerInRange.Length; i++)
         {
-            Transform player = playerInRange[i].transform;
-            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            Transform player = playerInRange[i].transform; //берем трансформ игрока
+            Vector3 dirToPlayer = (player.position - transform.position).normalized; // определяем направление к игроку
             if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
             {
                 float dstToPlayer = Vector3.Distance(transform.position, player.position);
-                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
+                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask)) //если препятствия не мешают видеть игрока
                 {
-                    m_PlayerInRange = true;
-                    m_IsPatrol = false;
+                    this.playerInRange = true; //погоня
+                    isPatrol = false;
                 }
                 else
                 {
-                    m_PlayerInRange = false;
+                    this.playerInRange = false; //остановка погони
                 }
             }
-
+            
+            // если убежали далеко
             if (Vector3.Distance(transform.position, player.position) > viewRadius)
             {
-                m_PlayerInRange = false;
+                Debug.Log("lost");
+                this.playerInRange = false;
+                isPatrol = true;
             }
-
-            if (m_PlayerInRange)
+            // находим позицию игрока
+            if (this.playerInRange)
             {
-                m_PlayerPosition = player.transform.position;
+                playerPosition = player.transform.position;
             }
         }
     }
